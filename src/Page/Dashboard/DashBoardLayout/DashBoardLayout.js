2e6ed46ad15468f781418639298dee84b78b218e
sharmin1988 <rasnasharmin11@gmail.com>
2022-11-26 00:24:40 +0600

    seller route , sellerHook, add

--- src/Page/Dashboard/DashBoardLayout/DashBoardLayout.js
+++ src/Page/Dashboard/DashBoardLayout/DashBoardLayout.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
 import useAdmin from '../../../hooks/useAdmin';
+import useSeller from '../../../hooks/useSeller';
 import Navbar from '../../Shared/Navbar/Navbar';
 
 
@@ -9,6 +10,8 @@ import Navbar from '../../Shared/Navbar/Navbar';
 const DashBoardLayout = () => {
     const { user } = useContext(AuthContext)
     const [isAdmin] = useAdmin(user?.email)
+    const [isSeller] = useSeller(user?.email)
+
     return (
         <div>
             <Navbar></Navbar>
@@ -20,14 +23,24 @@ const DashBoardLayout = () => {
                 <div className="drawer-side">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80  text-base-content">
-                        
-                        {isAdmin? <>
-                            <li><Link to='/dashboard/admin/allSellers'>All Sellers</Link></li>
-                            <li><Link to='/dashboard/admin/allBuyers'>All Buyers</Link></li>
-                            <li><Link to='/dashboard/admin/reportedItems'>Reported items</Link></li>
-                        </>
-                        :<li><Link to='/dashboard'>My Orders</Link></li>}
+                        {
+                            isAdmin && <>
+                                <li><Link to='/dashboard/admin/allSellers'>All Sellers</Link></li>
+                                <li><Link to='/dashboard/admin/allBuyers'>All Buyers</Link></li>
+                                <li><Link to='/dashboard/admin/reportedItems'>Reported items</Link></li>
+                            </>
+                        }
 
+                        {
+                            isSeller &&
+                            <>
+                                <li><Link to='/dashboard/admin/allSellers'>All products</Link></li>
+                                <li><Link to='/dashboard/admin/allBuyers'>All Buyers</Link></li>
+                                <li><Link to='/dashboard/admin/reportedItems'>Reported items</Link></li>
+                            </>
+                        }
+
+                        {!isAdmin && !isSeller && <li><Link to='/dashboard'>My Orders</Link></li>}
                     </ul>
 
                 </div>
--- src/Page/Dashboard/Dashboard/Dashboard.js
+++ src/Page/Dashboard/Dashboard/Dashboard.js
@@ -1,17 +1,19 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
 import useAdmin from '../../../hooks/useAdmin';
+import useSeller from '../../../hooks/useSeller';
 import AllSellers from '../Admin/AllSellers/AllSellers';
 import MyOrders from '../MyOrders/MyOrders';
 
 const Dashboard = () => {
     const { user } = useContext(AuthContext)
     const [isAdmin] = useAdmin(user?.email)
+    const [isSeller] = useSeller(user?.email)
     return (
         <div>
-            {
-                isAdmin?<AllSellers></AllSellers>:<MyOrders></MyOrders>
-            }
+            {isSeller && <AllSellers></AllSellers>}
+            {isAdmin && <AllSellers></AllSellers>}
+            {!isAdmin && !isSeller && <MyOrders></MyOrders>}
         </div>
     );
 };
